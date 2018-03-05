af58047ffca7fde71ce87fd19498068fb65a58db
Chelsea Conwell <cmconwel@my.loyno.edu>
2018-03-04 20:59:18 -0600

    Final Touches
    Editing the layout of pictures with paragraph and video. Changed the intro layout page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 var visibleText = "Show Less"
 var hiddenText = "Show More"
 $(document).ready(function () {
-    alert("hello");
     $(".info-box button").click(function () {
         $(this).toggleClass("visible");
         if ($(this).hasClass("visible")) {
